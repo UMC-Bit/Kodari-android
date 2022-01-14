fbb2f15ac4d8d303d66aa67f31fbe92530334547
junhyeok0112 <wd78wg@naver.com>
2022-01-15 02:11:29 +0900

    main , debate v1

--- app/src/main/java/com/bit/kodari/MainActivity.kt
+++ app/src/main/java/com/bit/kodari/MainActivity.kt
@@ -13,9 +13,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setListener()
+
         supportFragmentManager.beginTransaction().replace(R.id.main_container_fl, HomeFragment())
             .commitAllowingStateLoss()
 
+    }
+
+    fun setListener(){
+        binding.mainBottomnavigationBnv.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.myCoin -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container_fl, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.debate -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container_fl, DebateMainFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.feed -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_container_fl, SearchFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+                }
+
+                R.id.add_on -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_container_fl, LockerFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+                }
+                R.id.profile -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_container_fl, LockerFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+                }
+
+            }
+            false
+        }
     }
 
 }
