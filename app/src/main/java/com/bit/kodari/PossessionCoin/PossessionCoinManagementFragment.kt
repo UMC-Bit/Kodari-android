ace721faf7242755c168bad14167f2ba94aad9aa
BYEONGJUN KANG <lucid9dawn@gmail.com>
2022-01-20 22:54:08 +0900

    complete delete dialog, memo dialog

--- app/src/main/java/com/bit/kodari/PossessionCoin/PossessionCoinManagementFragment.kt
+++ app/src/main/java/com/bit/kodari/PossessionCoin/PossessionCoinManagementFragment.kt
@@ -1,9 +1,13 @@
 package com.bit.kodari.PossessionCoin
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.bit.kodari.Main.MainActivity
 import com.bit.kodari.R
@@ -20,85 +24,64 @@ class PossessionCoinManagementFragment : Fragment() {
 
         binding.possessionCoinManagementAddTV.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.temp, PossessionCoinSearchFragment()).commitAllowingStateLoss()
+                .replace(R.id.main_container_fl, PossessionCoinSearchFragment()).commitAllowingStateLoss()
         }
 
-        binding.tempDialogBT.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.temp, PossessionCoinMemoDialogFragment()).commitAllowingStateLoss()
-        }
+        memoDialog()
+
+        deleteDialog()
 
         return binding.root
     }
+
+    fun memoDialog()
+    {
+        binding.tempDialogBT.setOnClickListener {
+            val memoDialogView=LayoutInflater.from(context as MainActivity).inflate(R.layout.fragment_possession_coin_memo_dialog, null)
+            val memoDialogBuilder= AlertDialog.Builder(context as MainActivity)
+                .setView(memoDialogView)
+
+            memoDialogBuilder.show()
+
+//            val memoAlertDialog=memoDialogBuilder.show()
+
+            val memoOffButton=memoDialogView.findViewById<TextView>(R.id.possession_coin_dialog_memo_off_TV)
+            val memoOnButton=memoDialogView.findViewById<TextView>(R.id.possession_coin_dialog_memo_on_TV)
+            val twitterOffButton=memoDialogView.findViewById<TextView>(R.id.possession_coin_dialog_twitter_off_TV)
+            val twitterOnButton=memoDialogView.findViewById<TextView>(R.id.possession_coin_dialog_twitter_on_TV)
+
+            memoOffButton.setOnClickListener {
+                memoOnButton.visibility=View.VISIBLE
+                twitterOnButton.visibility=View.GONE
+            }
+            twitterOffButton.setOnClickListener {
+                twitterOnButton.visibility=View.VISIBLE
+                memoOnButton.visibility=View.GONE
+                memoOffButton.visibility=View.VISIBLE
+            }
+        }
+    }
+
+    fun deleteDialog()
+    {
+        binding.tempDeleteDialogBT.setOnClickListener {
+            val deleteDialogView=LayoutInflater.from(context as MainActivity).inflate(R.layout.fragment_possession_coin_delete_dialog, null)
+            val deleteDialogBuilder=AlertDialog.Builder(context as MainActivity)
+                .setView(deleteDialogView)
+
+            val deleteAlertDialog = deleteDialogBuilder.show()
+
+//
+            val deleteConfirmButton=deleteDialogView.findViewById<TextView>(R.id.possession_coin_delete_dialog_delete_confirm_TV)
+            val cancelButton=deleteDialogView.findViewById<TextView>(R.id.possession_coin_delete_dialog_cancel_TV)
+
+            deleteConfirmButton.setOnClickListener {
+                deleteAlertDialog.dismiss()
+            }
+
+            cancelButton.setOnClickListener {
+                deleteAlertDialog.dismiss()
+            }
+        }
+    }
 }
-
-//    fun setListener(){
-//        binding.possessionCoinManagementPresentPriceOffTV.setOnClickListener {
-//            binding.possessionCoinManagementPresentPriceOnTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementProfitOnBT.visibility=View.GONE
-//            binding.possessionCoinManagementAverageunitPriceOnBT.visibility=View.GONE
-//        }
-//
-//        binding.possessionCoinManagementProfitOffBT.setOnClickListener {
-//            binding.possessionCoinManagementProfitOnBT.visibility=View.VISIBLE
-//            binding.possessionCoinManagementPresentPriceOnTV.visibility=View.GONE
-//            binding.possessionCoinManagementPresentPriceOffTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementAverageunitPriceOnBT.visibility=View.GONE
-//        }
-//
-//        binding.possessionCoinManagementAverageunitPriceOffBT.setOnClickListener {
-//            binding.possessionCoinManagementAverageunitPriceOnBT.visibility=View.VISIBLE
-//            binding.possessionCoinManagementPresentPriceOnTV.visibility=View.GONE
-//            binding.possessionCoinManagementPresentPriceOffTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementProfitOnBT.visibility=View.GONE
-//        }
-//
-//        binding.possessionCoinManagementDeleteOffButtonIB.setOnClickListener {
-//            binding.possessionCoinManagementDeleteOnButtonIB.visibility=View.VISIBLE
-//            binding.possessionCoinManagementDeleteOffButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementAddTV.visibility=View.GONE
-//            binding.possessionCoinManagementDeleteConfirmButtonTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementModifyConfirmButtonTV.visibility=View.GONE
-//            binding.possessionCoinManagementModifyOnButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementModifyOffButtonIB.visibility=View.VISIBLE
-//        }
-//
-//        binding.possessionCoinManagementDeleteOnButtonIB.setOnClickListener {
-//            binding.possessionCoinManagementDeleteOffButtonIB.visibility=View.VISIBLE
-//            binding.possessionCoinManagementDeleteOnButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementAddTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementDeleteConfirmButtonTV.visibility=View.GONE
-//            binding.possessionCoinManagementModifyConfirmButtonTV.visibility=View.GONE
-//            binding.possessionCoinManagementModifyOnButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementModifyOffButtonIB.visibility=View.VISIBLE
-//        }
-//
-//        binding.possessionCoinManagementModifyOffButtonIB.setOnClickListener {
-//            binding.possessionCoinManagementModifyOnButtonIB.visibility=View.VISIBLE
-//            binding.possessionCoinManagementModifyOffButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementAddTV.visibility=View.GONE
-//            binding.possessionCoinManagementModifyConfirmButtonTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementDeleteConfirmButtonTV.visibility=View.GONE
-//            binding.possessionCoinManagementDeleteOnButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementDeleteOffButtonIB.visibility=View.VISIBLE
-//        }
-//
-//        binding.possessionCoinManagementModifyOnButtonIB.setOnClickListener {
-//            binding.possessionCoinManagementModifyOnButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementModifyOffButtonIB.visibility=View.VISIBLE
-//            binding.possessionCoinManagementAddTV.visibility=View.VISIBLE
-//            binding.possessionCoinManagementModifyConfirmButtonTV.visibility=View.GONE
-//            binding.possessionCoinManagementDeleteConfirmButtonTV.visibility=View.GONE
-//            binding.possessionCoinManagementDeleteOnButtonIB.visibility=View.GONE
-//            binding.possessionCoinManagementDeleteOffButtonIB.visibility=View.VISIBLE
-//        }
-//    }
-
-
-//override fun onCreateView(
-//    inflater: LayoutInflater, container: ViewGroup?,
-//    savedInstanceState: Bundle?
-//): View? {
-//    binding = FragmentPossessionCoinManagementBinding.inflate(inflater , container , false)
-//    return binding.root
-//}
