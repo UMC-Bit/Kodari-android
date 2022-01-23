aafdb75064968178d4f9f3d2a3101f63c990ab6d
BYEONGJUN KANG <lucid9dawn@gmail.com>
2022-01-23 21:11:34 +0900

    implementation date and time picker

--- app/src/main/java/com/bit/kodari/PossessionCoin/PossessionCoinAddFragment.kt
+++ app/src/main/java/com/bit/kodari/PossessionCoin/PossessionCoinAddFragment.kt
@@ -1,16 +1,23 @@
 package com.bit.kodari.PossessionCoin
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TimePicker
 import com.bit.kodari.Main.MainActivity
 import com.bit.kodari.R
 import com.bit.kodari.databinding.FragmentPossessionCoinAddBinding
 import com.bit.kodari.databinding.FragmentPossessionCoinManagementBinding
+import java.lang.StringBuilder
+import java.util.*
 
 class PossessionCoinAddFragment : Fragment() {
+    val tradeTime = StringBuilder();
     lateinit var binding:FragmentPossessionCoinAddBinding
 
     override fun onCreateView(
@@ -19,6 +26,48 @@ class PossessionCoinAddFragment : Fragment() {
     ): View? {
         binding = FragmentPossessionCoinAddBinding.inflate(inflater, container, false)
 
+        moveLayout()
+
+        datetimepicker()
+
+        return binding.root
+    }
+
+    fun datetimepicker()
+    {
+        val mcurrentTime = Calendar.getInstance()
+        val year = mcurrentTime.get(Calendar.YEAR)
+        val month = mcurrentTime.get(Calendar.MONTH)
+        val day = mcurrentTime.get(Calendar.DAY_OF_MONTH)
+
+        val mcurrent= Calendar.getInstance()
+        val hour = mcurrent.get(Calendar.HOUR_OF_DAY)
+        val minute = mcurrent.get(Calendar.MINUTE)
+
+        val timePicker = TimePickerDialog(context as MainActivity,
+            { view, hourOfDay, minute ->
+                val date = String.format("%02d:%02d", hourOfDay, minute)
+                tradeTime.append(date);
+                binding.possessionCoinAddDayInputET.setText(tradeTime.toString())
+                tradeTime.setLength(0)
+            }, hour, minute, false)
+
+        val datePicker = DatePickerDialog(context as MainActivity,
+            { view, year, month, dayOfMonth ->
+                val date = String.format("%d-%02d-%02d ", year, month + 1, dayOfMonth)
+                tradeTime.append(date)
+                tradeTime.append("")
+                timePicker.show()
+            }, year, month, day)
+
+        binding.possessionCoinAddDayInputET.setOnClickListener { v ->
+            datePicker.show()
+        }
+    }
+
+
+    fun moveLayout()
+    {
         binding.possessionCoinAddCompleteButtonTV.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
                 .replace(R.id.main_container_fl, PossessionCoinManagementFragment()).commitAllowingStateLoss()
@@ -28,7 +77,5 @@ class PossessionCoinAddFragment : Fragment() {
             (context as MainActivity).supportFragmentManager.beginTransaction()
                 .replace(R.id.main_container_fl, PossessionCoinSearchFragment()).commitAllowingStateLoss()
         }
-
-        return binding.root
     }
 }
