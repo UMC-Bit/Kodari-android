283848b7fbc290b2e28960d4602ba525eb4bd2e0
BYEONGJUN KANG <lucid9dawn@gmail.com>
2022-02-20 01:11:42 +0900

    preparation the addon, feed

--- app/src/main/java/com/bit/kodari/AddOn/AddOnMainFragment.kt
+++ app/src/main/java/com/bit/kodari/AddOn/AddOnMainFragment.kt
@@ -1,10 +1,16 @@
 package com.bit.kodari.AddOn
 
 import com.bit.kodari.Config.BaseFragment
+import com.bit.kodari.Main.HomeFragment
+import com.bit.kodari.Main.MainActivity
+import com.bit.kodari.R
 import com.bit.kodari.databinding.FragmentAddOnMainBinding
 
 class AddOnMainFragment : BaseFragment<FragmentAddOnMainBinding>(FragmentAddOnMainBinding::inflate) {
     override fun initAfterBinding() {
-
+        binding.addonMainBackbuttonIB.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container_fl, HomeFragment()).commitAllowingStateLoss()
+        }
     }
 }
--- app/src/main/java/com/bit/kodari/Feed/FeedMainFragment.kt
+++ app/src/main/java/com/bit/kodari/Feed/FeedMainFragment.kt
@@ -1,10 +1,16 @@
 package com.bit.kodari.Feed
 
 import com.bit.kodari.Config.BaseFragment
+import com.bit.kodari.Main.HomeFragment
+import com.bit.kodari.Main.MainActivity
+import com.bit.kodari.R
 import com.bit.kodari.databinding.FragmentFeedMainBinding
 
 class FeedMainFragment : BaseFragment<FragmentFeedMainBinding>(FragmentFeedMainBinding::inflate) {
     override fun initAfterBinding() {
-
+        binding.feedMainBackbuttonIB.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container_fl, HomeFragment()).commitAllowingStateLoss()
+        }
     }
 }
