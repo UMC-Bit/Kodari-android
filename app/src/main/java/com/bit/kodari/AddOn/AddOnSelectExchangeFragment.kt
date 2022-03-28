cfa01f94e6071d0b849f3b1abc6acdb673180b0b
junhyeok0112 <wd78wg@naver.com>
2022-03-28 22:50:06 +0900

    [레이아웃구현] 애드온 4가지 레이아웃 구현

---
+++ app/src/main/java/com/bit/kodari/AddOn/AddOnSelectExchangeFragment.kt
@@ -0,0 +1,18 @@
+package com.bit.kodari.AddOn
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bit.kodari.Config.BaseFragment
+import com.bit.kodari.R
+import com.bit.kodari.databinding.FragmentAddOnSelectExchangeBinding
+
+//애드온 거래소 선택 화면
+class AddOnSelectExchangeFragment : BaseFragment<FragmentAddOnSelectExchangeBinding>(FragmentAddOnSelectExchangeBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+}
---
+++ app/src/main/java/com/bit/kodari/AddOn/DialogAddOnSelectCoin.kt
@@ -0,0 +1,12 @@
+package com.bit.kodari.AddOn
+
+import com.bit.kodari.Config.BaseDialogFragment
+import com.bit.kodari.databinding.DialogAddonSelectCoinBinding
+
+//애드온 코인 선택 화면
+class DialogAddOnSelectCoin : BaseDialogFragment<DialogAddonSelectCoinBinding>(DialogAddonSelectCoinBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+}
