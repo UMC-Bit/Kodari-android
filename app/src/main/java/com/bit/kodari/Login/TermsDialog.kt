9ce349bf6ba5a7da25e0aacbe5e69c3d54128fc3
junhyeok0112 <wd78wg@naver.com>
2022-04-07 00:18:37 +0900

    [버그수정] 이용약관 레이아웃 수정

--- app/src/main/java/com/bit/kodari/Login/TermsDialog.kt
+++ app/src/main/java/com/bit/kodari/Login/TermsDialog.kt
@@ -9,41 +9,43 @@ import android.icu.util.TimeZone
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.DialogFragment
+import com.bit.kodari.Config.BaseDialogFragment
 import com.bit.kodari.R
 import com.bit.kodari.databinding.DialogTermsBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.util.*
 
-class TermsDialog : DialogFragment() {
-    lateinit var binding : DialogTermsBinding
+class TermsDialog : BaseDialogFragment<DialogTermsBinding>(DialogTermsBinding::inflate) {
     var check = false
     lateinit var size : Point
     lateinit var termsView: TermsView
 
-    override fun onResume() {
+        override fun onResume() {
         super.onResume()
         //여백 비율로 DialogFragment 크기 조절
         val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
         val deviceWidth = size.x
         val deviceHeight = size.y
         params?.width = (deviceWidth * 0.8).toInt()
-        params?.height = (deviceHeight* 0.7).toInt()
+        params?.height = (deviceHeight* 0.8).toInt()
         dialog?.window?.attributes = params as WindowManager.LayoutParams
 //
 //        dialog!!.window!!.setLayout(
 //            WindowManager.LayoutParams.WRAP_CONTENT,
 //            WindowManager.LayoutParams.WRAP_CONTENT)
     }
+
+    override fun initAfterBinding() {
+        getDeviceSize()
+        setLister()
+    }
+
     fun setTermsInterface(termsView: TermsView){
         this.termsView = termsView
     }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View?{
-        binding = DialogTermsBinding.inflate(inflater , container, false)
-        getDeviceSize()
+
+
+    fun setLister(){
         binding.termsSuccessBtn.setOnClickListener {
             termsView.setAgree(true)
             dismiss()
@@ -51,10 +53,8 @@ class TermsDialog : DialogFragment() {
         binding.termsExit.setOnClickListener {
             dismiss()
         }
-        dialog?.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
-        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        return binding.root
     }
+
     //디바이스 크기 구하기
     fun getDeviceSize(){
         val windowManager = requireActivity().getSystemService(Context.WINDOW_SERVICE) as WindowManager
